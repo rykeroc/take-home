bb925a69341ce00ef9f4f0c51e815665f9daa230
rykeroc <ryker.cooke@gmail.com>
2025-09-29 15:31:45 -0400

    Add logger
    Add error logging on error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,17 +3,23 @@
 import { cn } from '@/lib/utils/tailwind';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { logger } from '@/lib/logger';
 
-export default function GlobalError() {
+export default function GlobalError({ error, reset }: { error: Error; reset: () => void }) {
+	useEffect(() => {
+		logger.error(error);
+	}, [error])
+
+	const handleReset = () => reset
+
 	return (
 		<section>
 			<div className={cn('mb-4', 'text-start', 'flex', 'flex-col', 'gap-2')}>
 				<h1>Something Went Wrong</h1>
 				<p>Sorry, an unexpected error has occurred. Please try again later.</p>
 			</div>
-			<Button asChild>
-				<Link href={'/'}>Return Home</Link>
-			</Button>
+			<Button onClick={handleReset}>Try again</Button>
 		</section>
 	);
 }
